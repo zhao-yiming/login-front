dcfd490063f48f765f9fda662234850cb4a146d0
zhao-yiming <zhao.yiming423@gmail.com>
2023-11-08 21:49:32 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [inputUser, setInputUser] = useState("");
+  const [inputPass, setInputPass] = useState("");
+
+
+  function login(){
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: inputUser,
+        password: inputPass
+      }),
+    }).then(response => {
+      return response.json()
+    })
+    .then(data => {
+      if (data.length>0){
+        setMessage("your information is "+ JSON.stringify(data));
+        
+        
+      }
+      else{
+        setMessage("access denied, please recheck your username or password")
+      }
+      
+    });
+
+    
+    
+  } 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     
+				<div> 
+					<label htmlFor="username">Username</label>
+					<input type="text" name="username" id="username" onChange={(e) => setInputUser(e.target.value)}/> 
+				</div> 
+				<div> 
+					<label htmlFor="passw">Password</label>
+					<input type="text" name="passw" id="passw" onChange={(e) => setInputPass(e.target.value)}/> 
+				</div>  
+				<button type="submit" onClick={login}>Login</button>
+        <div>{message}</div>
+		
     </div>
   );
 }
